13ddaba115c304c8440f778c5b4ea7d91dcc3440
Keita Ito <eguchi@eguchi.cc>
2024-08-16 20:12:56 +0900

    タスク定義の作成

--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,23 @@
 resource "aws_ecs_cluster" "this" {
   name = "cluster"
 }
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = "service"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions = jsonencode([
+    {
+      name  = "hello-world"
+      image = "docker/welcome-to-docker"
+      "portMappings" : [
+        {
+          "containerPort" : 80,
+          "hostPort" : 80,
+          "protocol" : "tcp",
+        }
+      ],
+    }
+  ])
+  cpu    = "512"
+  memory = "1024"
+}
